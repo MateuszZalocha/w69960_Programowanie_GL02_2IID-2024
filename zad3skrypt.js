5b4d6263c6ec1d17fc36ed2927cac3bac5295846
Mateusz Załocha <w69960@student.wsiz.edu.pl>
2024-03-22 12:16:20 +0100

    iht

---
+++ zad3skrypt.js
@@ -0,0 +1,18 @@
+function zmienTekst() {
+    alert("Witaj");
+}
+
+
+  
+function liczbyparzyste()
+    let x=0
+    while(x<=100){
+        console.log(x)
+        x=x+2
+    }
+function dodaj(){
+    let a=prompt("podaj liczbe")
+    let b=prompt("podaj liczbe")
+    let c=parseInt(a)+parseInt(b)
+    document.write(c)
+}
